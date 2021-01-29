375eb9805f8296dd44b52b8d8410e2baaaf8f3dd
wy672515431 <672515431@qq.com>
2021-01-29 19:21:09 +0800

    增加了简单的敌人
    1

--- Assets/Enemy/EnemyProducer.cs
+++ Assets/Enemy/EnemyProducer.cs
@@ -4,21 +4,60 @@ using UnityEngine;
 public class EnemyProducer : MonoBehaviour
 {
     public GameObject Enemy;
+
+    //敌人的最大数量
+    public int maxcount = 3;
+    
+    //当前的敌人数量
+    public int realcount;
+
+    //当前的敌人数目
+    public int startcount = 3;
+
+    //生成敌人的延迟时间
+    public float maxTime = 1.5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        GameObject.Find("EnemyProductButton").GetComponent<EnemyProductButton>().EP += Product;
+        for (int i = 0; i < startcount; i++)
+        {
+            GenerateEnemy();
+        }
+
+        StartCoroutine(Logic());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-    public void Product(){
-        int x = Random.Range(-5,5);
-        int y = Random.Range(-5,5);
-        Vector3 position = new Vector3(x,y,0);
-        Instantiate(Enemy,position,transform.rotation);
+
+    public void GenerateEnemy()
+    {
+        if (realcount < maxcount)
+        {
+            Invoke("CreateEnemy", Random.Range(0, maxTime));
+            realcount++;
+        }
+    }
+
+    public void CreateEnemy()
+    {
+        Instantiate(Enemy, new Vector3(Random.Range(-10.0f, 5.0f), Random.Range(-10.0f, 5.0f)), Quaternion.identity);
+    }
+
+    IEnumerator Logic()
+    {
+        while (true)
+        {
+            if (realcount < maxcount)
+            {
+                Instantiate(Enemy, new Vector3(Random.Range(-10.0f, 5.0f), Random.Range(-10.0f, 5.0f)), Quaternion.identity);
+                realcount++;
+            }
+            yield return new WaitForSeconds(maxTime);
+        }
     }
 }
